2ef46d55eb0e7bfbadb240e544d3dca6d3fdce93
dahunsi halima <dahunsiolajumoke18@gmail.com>
2025-07-28 17:25:36 +0100

    generated different movies for the library

--- script.js
+++ script.js
@@ -1,11 +1,47 @@
+class Movie {
+    constructor(title, director, year) {
+        this.title = title;
+        this.director = director;
+        this.year = year;
+    }
+
+    getDetails() {
+        return `${this.title} (${this.year}) by ${this.director}`;
+    }
+}
+
+class ComedyMovie extends Movie {
+    constructor(title, director, year, leadComedian) {
+        super(title, director, year);
+        this.leadComedian = leadComedian;
+    }
+
+    getDetails() {
+        return `${super.getDetails()} - Comedy starring ${this.leadComedian}`;
+    }
+}
+
+class ActionMovie extends Movie {
+    constructor(title, director, year, stuntCoordinator) {
+        super(title, director, year);
+        this.stuntCoordinator = stuntCoordinator;
+    }
+
+    getDetails() {
+        return `${super.getDetails()} - Action with stunts by ${this.stuntCoordinator}`;
+    }
+}
+
 const movieLibrary = [];
 
 // 15 Regular Movies
-movieLibrary.push(new Movie("The Silent Echo", "Jane Smith", 2010));
-movieLibrary.push(new Movie("Beyond the Horizon", "Alan Rickman", 2011));
-movieLibrary.push(new Movie("Whispers of Time", "Mia Turner", 2012));
-movieLibrary.push(new Movie("Echoes of War", "Jake Carson", 2013));
-movieLibrary.push(new Movie("Moonlight Diary", "Zara Holt", 2014));
+movieLibrary.push(new Movie("The Silent Echo", "Jane Smith", 2010, "https://media.istockphoto.com/id/483232763/photo/youtube-logo.jpg?s=612x612&w=0&k=20&c=JjGpvabBYTfbvaZGTxzZsjZd30tlrcdvRJueeP8VVCQ="));
+movieLibrary.push(new Movie("Beyond the Horizon", "Alan Rickman", 2011, "https://media.istockphoto.com/id/2186789147/photo/3d-render-cyborg-woman-stands-in-an-attacking-pose-with-an-assault-gun-in-one-hand.jpg?s=612x612&w=0&k=20&c=HCacXJiqEMTnHkEX5EJAzQkqB0DAzbManQHfjMn1Vbg="));
+movieLibrary.push(new Movie("Whispers of Time", "Mia Turner", 2012, "https://media.istockphoto.com/id/1393151713/vector/karate-kick-martial-arts-inscription-on-illustration-is-a-hieroglyphs-of-karate-japanese.jpg?s=612x612&w=0&k=20&c=Xd7wqBohRhJBNkeAWwJvvo1fQupuU2qJPmUzkbSjRMM="));
+movieLibrary.push(new Movie("Echoes of War", "Jake Carson", 2013, "https://media.istockphoto.com/id/1520056460/photo/thriller.jpg?s=612x612&w=0&k=20&c=mCyJukUXEkhWYZl8dIGXfiWv68aqUarJdVWZnCB9OLs="));
+movieLibrary.push(new Movie("Moonlight Diary", "Zara Holt", 2014, "https://cdn.pixabay.com/photo/2023/07/22/04/15/motorbike-8142649_640.jpg"));
+
+
 movieLibrary.push(new Movie("Winter’s Code", "Leo Grant", 2015));
 movieLibrary.push(new Movie("Glass Veil", "Emily Rhodes", 2016));
 movieLibrary.push(new Movie("Chronicles of Now", "Dean Wallace", 2017));
@@ -45,3 +81,15 @@ movieLibrary.push(new ActionMovie("Zero Gravity", "Dean Frost", 2024, "Vic Shado
 movieLibrary.push(new ActionMovie("Dark Pursuit", "Nico Ray", 2021, "Stunt King"));
 movieLibrary.push(new ActionMovie("Ghost Ops", "Finn Moore", 2022, "Rico Silva"));
 movieLibrary.push(new ActionMovie("Battle Core", "Tracy Miles", 2023, "Eva Flash"));
+
+
+console.log(movieLibrary)
+
+
+const actionMovies = movieLibrary.filter(movie => movie instanceof ComedyMovie);
+actionMovies.forEach(movie => {
+    console.log(movie.getDetails());
+});
+
+const movies = movieLibrary.movie.map(item, index)
+console.log(movies)
